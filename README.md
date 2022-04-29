287141d11a9522f42cb53e412d4675cbde27c659
lihuaxin <admin@192.168.0.104>
2022-04-29 22:12:34 +0800

    demo

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # npm-test
 publish npm package
+
+
+[在线文档链接](http://121.37.159.50:9009/)
+
+## usage
+
+## 使用
+
+- Commands
+  - **`lerna publish`**: Create a new release of the packages that have been updated. Prompts for a new version and updates all the packages on git and npm
+  - [`lerna version`](./commands/version#readme)
+  - **`lerna bootstrap`**：Bootstrap the packages in the current Lerna repo. Installing all their dependencies and linking any cross-dependencies.
+  - **`lerna list`**:List all of the public packages in the current Lerna repo.
+  - **`lerna changed`**:Check which packages have changed since the last release.
+  - **`lerna diff`**:Diff all packages or a single package since the last release.
+  - [`lerna exec`](./commands/exec#readme)
+  - **`lerna run`**:Run an npm script in each package that contains that script.
+  - **`lerna init`**：Create a new lerna repo or upgrade an existing repo to the current version of Lerna.
+  - [`lerna add`](./commands/add#readme)
+  - [`lerna clean`](./commands/clean#readme)
+  - **`lerna import`**：Import the package in the local path `<pathToRepo>` into `packages/<directory-name>` with commit history.
+  - [`lerna link`](./commands/link#readme)
+  - [`lerna create`](./commands/create#readme)
+  - [`lerna info`](./commands/info#readme)
+
+# documents
+
+- [lerna](https://github.com/lerna/lerna)
+- [verdaccio](https://verdaccio.org/docs/en/what-is-verdaccio)
+- [nrm](https://www.npmjs.com/package/nrm)
---
+++ packages/hooks/src/useEnqueueTask/index.ts
@@ -0,0 +1,58 @@
+import { useInterval } from "ahooks";
+import { useCallback, useRef } from "react";
+
+type Props<T> = {
+  delay?: number;
+  maxSize?: number;
+  taskHandler: (data: T) => void;
+  fetchHandler: () => Promise<T>;
+};
+
+export const useEnqueueTask = <T = any>({
+  taskHandler,
+  fetchHandler,
+  delay = 3000,
+  maxSize = 20,
+}: Props<T>) => {
+  const taskList = useRef<T[]>([]);
+  const idleCallbackRef = useRef<number>(0);
+
+  const runTaskQueue = (deadline: IdleDeadline) => {
+    while (
+      (deadline.timeRemaining() > 0 || deadline.didTimeout) &&
+      taskList.current.length
+    ) {
+      const data = taskList.current.shift() as T;
+      taskHandler(data);
+    }
+
+    if (taskList.current.length) {
+      idleCallbackRef.current = requestIdleCallback(runTaskQueue);
+    } else {
+      idleCallbackRef.current = 0;
+    }
+  };
+
+  const resetTask = () => {
+    cancelIdleCallback(idleCallbackRef.current);
+    taskList.current.length = 0;
+    idleCallbackRef.current = 0;
+  };
+
+  const enqueueTask = (taskData: T) => {
+    if (taskList.current.length > maxSize) {
+      resetTask();
+    }
+    taskList.current.push(taskData);
+    if (!idleCallbackRef.current) {
+      idleCallbackRef.current = requestIdleCallback(runTaskQueue);
+    }
+  };
+
+  const intervalHandler = useCallback(async () => {
+    const data = await fetchHandler();
+    enqueueTask(data);
+  }, []);
+
+  useInterval(intervalHandler, delay, { immediate: true });
+};
